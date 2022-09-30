4094e16954ce8eafc13b8cc3c908a37ab17b00f1
Matteo Foresti <mforesti@datacolor.com>
2022-09-30 17:54:17 +0200

    change from gt3 to lt2

--- TestGit/TestGit/TestGit.cpp
+++ TestGit/TestGit/TestGit.cpp
@@ -9,12 +9,12 @@ int main()
     int n1 = 3;
     int n2 = 6;
     auto is_even = [](int i) { return i % 2 == 0; };
-    auto is_gt3 = [](int i) { return i > 3; };
+    auto is_lt2 = [](int i) { return i < 2; };
 
     auto result1 = std::find(begin(v), end(v), n1);
     auto result2 = std::find(begin(v), end(v), n2);
     auto result3 = std::find_if(begin(v), end(v), is_even);
-    auto result4 = std::find_if(begin(v), end(v), is_gt3);
+    auto result4 = std::find_if(begin(v), end(v), is_lt2);
 
     (result1 != std::end(v))
         ? std::cout << "v contains " << n1 << '\n'
@@ -29,6 +29,6 @@ int main()
         : std::cout << "v does not contain even numbers\n";
 
     (result4 != std::end(v))
-        ? std::cout << "v contains a number greater than 3: " << *result4 << '\n'
-        : std::cout << "v does not contain a number greater than 3\n";
+        ? std::cout << "v contains a number less than 2: " << *result4 << '\n'
+        : std::cout << "v does not contain a number less than 2\n";
 }
